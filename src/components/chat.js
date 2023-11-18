370920665371107e9d7e4064effe7650a1f106ed
P Srikrishna Santosh <112562034+Sri-1203@users.noreply.github.com>
2023-11-18 22:30:05 +0530

    Update chat.js

--- src/components/chat.js
+++ src/components/chat.js
@@ -55,12 +55,14 @@ const Chat = ({ location }) => {
 
   return (
     <div className="outerContainer">
+    <TextContainer users={users}/>
+    
     <div className="container">
         <InfoBar room={room} />
         <Messages messages={messages} name={name} />
         <Input message={message} setMessage={setMessage} sendMessage={sendMessage} />
     </div>
-    <TextContainer users={users}/>
+   
   </div>
   );
 }
